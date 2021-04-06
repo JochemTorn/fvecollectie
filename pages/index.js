7b20b85ab112163a4292e902ae6831d12e941640
JochemTorn <jochemtorn@gmail.com>
2021-04-06 21:37:28 +0200

    responsive title

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Artwork from '../components/Artwork'
 import Layout from '../components/Layout'
+import Nav from '../components/Nav'
 const Sitetitle = "De FVE Collectie 2020/2021"
 
 export default function Home() {
@@ -11,8 +12,8 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
       </Head>
         <Layout>
-          <div className="h-28 flex items-center justify-center">
-            <h1 className="text-4xl font-medium">{Sitetitle}</h1>
+          <div className="h-28 flex items-center justify-start">
+            <h1 className="text-2xl sm:text-3xl md:text-4xl font-medium">{Sitetitle}</h1>
           </div>
           <div className="justify-items-center grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
             <Artwork src="1.jpeg"></Artwork>
